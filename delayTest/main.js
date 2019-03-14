acafcb479c6fec6c0af40fc116f072012c3f4ef7
jianxun <jianxun@guanghe.tv>
2019-03-14 23:00:11 +0800

    [feat][jianxun] add node delay message test code

---
+++ delayTest/main.js
@@ -0,0 +1,19 @@
+const axios = require('axios')
+const moment = require('moment')
+const Promise = require('bluebird')
+
+const main = async () => {
+    const DIFF = 30
+    const BATCH_NUM= 100
+    const format = 'YYYY-MM-DD HH:mm:ss'
+    const nowStr = moment().add(DIFF, 's').format(format)
+    const url = `http://localhost:9090/delay?time=${nowStr}`
+    const arr = []
+    for(let i = 0; i < BATCH_NUM; i += 1) {
+        console.log(url)
+        arr.push(url)
+    }
+    await Promise.map(arr, async u => axios.get(u), { concurrency: 20 })
+}
+
+main().then()
