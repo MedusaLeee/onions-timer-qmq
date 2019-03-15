4ce1212f638d2a810383347255131844c06c32b3
jianxun <jianxun@guanghe.tv>
2019-03-15 10:40:41 +0800

    [feat][jianxun] add sleep to start and stop qmq shell

--- startQmq.sh
+++ startQmq.sh
@@ -3,6 +3,9 @@
 cd ~/tools/qmq-dist-1.1.3.5-bin/bin
 
 ./metaserver.sh start
+sleep 2s
 ./broker.sh start
+sleep 1s
 ./delay.sh start
+sleep 1s
 ./watchdog.sh start
--- stopQmq.sh
+++ stopQmq.sh
@@ -3,6 +3,9 @@
 cd ~/tools/qmq-dist-1.1.3.5-bin/bin
 
 ./watchdog.sh stop
+sleep 1s
 ./delay.sh stop
+sleep 1s
 ./broker.sh stop
+sleep 1s
 ./metaserver.sh stop
