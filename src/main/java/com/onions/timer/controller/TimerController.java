df302f0c4c89c3d3d3f1f455078250d4fc055dab
jianxun <jianxun@guanghe.tv>
2019-03-26 21:01:20 +0800

    [feat][jianxun] modify test message route

--- src/main/java/com/onions/timer/controller/TimerController.java
+++ src/main/java/com/onions/timer/controller/TimerController.java
@@ -1,5 +1,6 @@
 package com.onions.timer.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.onions.timer.dto.MessageDto;
 import com.onions.timer.service.QmqService;
 import org.apache.commons.lang3.time.DateUtils;
@@ -14,24 +15,32 @@ import org.springframework.web.context.request.async.DeferredResult;
 
 import java.text.ParseException;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 public class TimerController {
     @Autowired
     private QmqService qmqService;
-
-    //    // 压力测试用的
-//    @GetMapping(path = "/messages")
-//    public DeferredResult<String> sendDelayAsyncMessage(String time, int index) {
-//        Date date;
-//        try {
-//            date = DateUtils.parseDate(time, "yyyy-MM-dd HH:mm:ss");
-//        } catch (ParseException e) {
-//            throw new Error("日期格式错误");
-//        }
-//        return qmqService.sendDelayAsyncMessage("message index: " + index + ", time: " + time, date);
-//    }
+    // 压力测试用的
+    @GetMapping(path = "/timers/test")
+    public DeferredResult<ResponseEntity<Object>> sendDelayAsyncMessage(String appId, String time) {
+        MessageDto messageDto;
+        try {
+            DateUtils.parseDate(time, "yyyy-MM-dd HH:mm:ss");
+            messageDto = new MessageDto();
+            messageDto.setAppId(appId);
+            messageDto.setTime(System.currentTimeMillis());
+            messageDto.setJobAt(time);
+            Map<String, String> map = new HashMap<>();
+            map.put("id", "abc");
+            map.put("name", "tom");
+            messageDto.setData(JSONObject.toJSONString(map));
+        } catch (ParseException e) {
+            throw new Error("日期格式错误");
+        }
+        return qmqService.sendDelayAsyncMessage(messageDto);
+    }
     @PostMapping(path = "/timers")
     public DeferredResult<ResponseEntity<Object>> addDelayMessage(@RequestBody MessageDto messageDto) {
         return qmqService.sendDelayAsyncMessage(messageDto);
